33c37a48f3255cccbe476dc406f78ca90036726f
Xiaoyang <xdong88@wisc.edu>
2023-10-18 23:19:24 -0500

    update rm_.sh

--- rm_n.sh
+++ rm_n.sh
@@ -1,1 +1,12 @@
 #!/bin/bash
+
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 <dir> <n>" 1>&2
+    exit 1
+fi
+
+dir="$1"
+size_limit="$2"
+
+find "$dir" -type f -size +"$size_limit"c -delete
+echo "Files larger than $size_limit bytes in '$dir' have been removed."
