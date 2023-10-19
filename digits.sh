e116c5aa82b8d207fa2573a9a8257f1dec769b5e
Ziming <zli2543@wisc.edu>
2023-10-18 22:26:51 -0500

    updated digits.sh

--- digits.sh
+++ digits.sh
@@ -1,1 +1,11 @@
 #!/bin/bash
+sum=0
+for num in {1000..2000}; do
+    if [[ $num =~ ^[01]+$ ]]; then
+        sum=$((sum + num))
+    fi
+done
+
+echo "The sum of numbers in range(1000,2000)\
+ having digits from\
+ the {0, 1} is: $sum"
