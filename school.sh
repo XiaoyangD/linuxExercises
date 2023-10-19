6e1cfbe2d0c49ee5c1aeb480cd43f20cf61fbac1
Ziming <zli2543@wisc.edu>
2023-10-18 22:10:57 -0500

    Updated school.sh

--- school.sh
+++ school.sh
@@ -1,1 +1,14 @@
 #!/bin/bash
+
+cat Property_Tax_Roll.csv | grep "MADISON SCHOOLS" | cut -d',' -f7 | 
+{
+    sum=0
+    count=0
+    while read value; do
+        sum=$(echo "$sum + $value" | bc)
+        count=$((count+1))
+    done
+    average=$(echo "scale=2; $sum / $count" | bc)
+    echo "Total Sum: $sum"
+    echo "Average: $average"
+}
